b8b6dcd3d6cb96b81485c89e7abfa91f5a1d874d
thiago.souzapassos <thiago.souzapassos@fe36a3be-8b6b-11de-91c6-75d7f44f5486>
2009-11-13 23:02:49 +0000

    Adicionando marcador no ponto numero 10

---
+++ trunk/WhereAmI5/src/org/thiagosouza/whereami/MyOverlay.java
@@ -0,0 +1,37 @@
+package org.thiagosouza.whereami;
+
+import java.util.ArrayList;
+
+import android.graphics.drawable.Drawable;
+
+import com.google.android.maps.ItemizedOverlay;
+import com.google.android.maps.OverlayItem;
+
+public class MyOverlay extends ItemizedOverlay {
+
+	private ArrayList<OverlayItem> mOverlays = new ArrayList<OverlayItem>();
+
+	public MyOverlay(Drawable defaultMarker) {
+		// super(defaultMarker);
+		super(boundCenterBottom(defaultMarker));
+	}
+
+	@Override
+	protected OverlayItem createItem(int i) {
+		// return null;
+		return mOverlays.get(i);
+
+	}
+
+	@Override
+	public int size() {
+		// return 0;
+		return mOverlays.size();
+	}
+
+	public void addOverlay(OverlayItem overlay) {
+		mOverlays.add(overlay);
+		populate();
+	}
+
+}
