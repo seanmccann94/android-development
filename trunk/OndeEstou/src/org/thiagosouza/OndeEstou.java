e224b35da4830b3cf1f997630b3caa760d337a80
thiago.souzapassos <thiago.souzapassos@fe36a3be-8b6b-11de-91c6-75d7f44f5486>
2009-11-15 21:34:19 +0000

    Initial import.

---
+++ trunk/OndeEstou/src/org/thiagosouza/OndeEstou.java
@@ -0,0 +1,168 @@
+package org.thiagosouza;
+
+import java.io.IOException;
+
+import java.util.List;
+import java.util.Locale;
+
+import com.google.android.maps.GeoPoint;
+import com.google.android.maps.MapActivity;
+import com.google.android.maps.MapController;
+import com.google.android.maps.MapView;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.Context;
+import android.location.Address;
+import android.location.Criteria;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class OndeEstou extends MapActivity {
+
+	LocationManager locationManager = null;
+	MapController mapController;
+
+	@Override
+	public void onCreate(Bundle icicle) {
+		super.onCreate(icicle);
+		setContentView(R.layout.main);
+
+		MapView myMapView = (MapView) findViewById(R.id.myMapView);
+		mapController = myMapView.getController();
+
+		// Configura opcoes de visualizacao do mapa
+		myMapView.setSatellite(true);
+		myMapView.setStreetView(true);
+
+		// Configura o zoom no mapa (21-1)
+		mapController.setZoom(16);
+
+		locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+
+		/*
+		 * Criteria criteria = new Criteria();
+		 * criteria.setAccuracy(Criteria.ACCURACY_FINE);
+		 * criteria.setAltitudeRequired(false);
+		 * criteria.setBearingRequired(false); criteria.setCostAllowed(true);
+		 * criteria.setPowerRequirement(Criteria.POWER_LOW); String provider =
+		 * locationManager.getBestProvider(criteria, true); String provider =
+		 * locationManager.getBestProvider(criteria, true);
+		 */
+
+		// Location location = locationManager.getLastKnownLocation(provider);
+
+		String provider = LocationManager.GPS_PROVIDER;
+
+		Location location = locationManager.getLastKnownLocation(provider);
+
+		updateWithNewLocation(location);
+
+		System.out.println(location.toString());
+
+		locationManager.requestLocationUpdates(provider, 2000, 10,
+				locationListener);
+
+		// chama a funcao para configurar os alertas de proximidade
+		setProximityAlert();
+	}
+
+	private final LocationListener locationListener = new LocationListener() {
+		public void onLocationChanged(Location location) {
+			updateWithNewLocation(location);
+		}
+
+		public void onProviderDisabled(String provider) {
+			updateWithNewLocation(null);
+		}
+
+		public void onProviderEnabled(String provider) {
+		}
+
+		public void onStatusChanged(String provider, int status, Bundle extras) {
+		}
+	};
+
+	/** Atualiza o mapa */
+	private void updateWithNewLocation(Location location) {
+		TextView myLocationText = (TextView) findViewById(R.id.myLocationText);
+
+		String latLongString;
+		String addressString = "Endereço não encontrado";
+
+		if (location != null) {
+
+			// Atualiza o mapa com a localizacao atual
+			Double geoLat = location.getLatitude() * 1E6;
+			Double geoLng = location.getLongitude() * 1E6;
+			GeoPoint point = new GeoPoint(geoLat.intValue(), geoLng.intValue());
+
+			mapController.animateTo(point);
+
+			double lat = location.getLatitude();
+			double lng = location.getLongitude();
+			latLongString = "Lat:" + lat + ", Long:" + lng;
+
+			Geocoder gc = new Geocoder(this, Locale.getDefault());
+			try {
+				List<Address> addresses = gc.getFromLocation(lat, lng, 1);
+				StringBuilder sb = new StringBuilder();
+				if (addresses.size() > 0) {
+					Address address = addresses.get(0);
+
+					for (int i = 0; i < address.getMaxAddressLineIndex(); i++) {
+						if (i == (address.getMaxAddressLineIndex() - 1))
+							sb.append(address.getAddressLine(i));
+						else
+							sb.append(address.getAddressLine(i)).append(" - ");
+					}
+				}
+				addressString = sb.toString();
+			} catch (IOException e) {
+			}
+		} else {
+			latLongString = "Localização não encontrada";
+		}
+
+		myLocationText.setText("Sua posição atual é:\n" + latLongString);
+
+		Toast.makeText(OndeEstou.this, addressString, Toast.LENGTH_LONG).show();
+
+	}
+
+	@Override
+	protected boolean isRouteDisplayed() {
+		return false;
+	}
+
+	public static void alert(Context context, String msg, String title) {
+
+		Dialog dialog = new AlertDialog.Builder(context).setIcon(0).setTitle(
+				title).setPositiveButton("OK", null).setMessage(msg).create();
+
+		dialog.show();
+	}
+
+	/** Configura um alerta de aproximacao */
+	private void setProximityAlert() {
+
+		ProximityAlert proximityAlertPonto1 = new ProximityAlert(-25.443195,
+				-49.280977, 100, "Alerta personalizado");
+
+		ProximityAlert proximityAlertPonto10 = new ProximityAlert(-25.442595,
+				-49.279444, 100);
+
+		Setup.addProximityAlert(proximityAlertPonto1);
+		Setup.addProximityAlert(proximityAlertPonto10);
+
+		@SuppressWarnings("unused")
+		Setup setup = new Setup(getApplicationContext(), locationManager);
+
+	}
+
+}
---
+++ trunk/OndeEstou/src/org/thiagosouza/ProximityAlert.java
@@ -0,0 +1,23 @@
+package org.thiagosouza;
+
+public class ProximityAlert {
+
+	Double lat = 0.0;
+	Double lng = 0.0;
+	float radius = 0;
+	String msg = "";
+	
+	ProximityAlert(Double lat, Double lng, float radius, String msg){
+		
+		this.lat = lat;
+		this.lng = lng;
+		this.radius = radius;
+		this.msg = msg;
+		
+	}
+	
+	ProximityAlert(Double lat, Double lng, float radius) {
+		this(lat, lng, radius, "");
+	}
+	
+}
