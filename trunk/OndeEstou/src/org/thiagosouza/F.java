bf60032d8656d16b78401b5cc861c33fa8b7afb3
thiago.souzapassos <thiago.souzapassos@fe36a3be-8b6b-11de-91c6-75d7f44f5486>
2009-11-16 11:30:51 +0000

    Comentando codigo e setando o MapView

---
+++ trunk/OndeEstou/src/org/thiagosouza/F.java
@@ -0,0 +1,23 @@
+package org.thiagosouza;
+
+import java.text.DecimalFormat;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.Context;
+
+public class F {
+
+	public static double roundSixDecimals(double d) {
+		DecimalFormat twoDForm = new DecimalFormat("#.######");
+		return Double.valueOf(twoDForm.format(d));
+	}
+
+	public static void alert(Context context, String msg, String title) {
+
+		Dialog dialog = new AlertDialog.Builder(context).setIcon(0).setTitle(
+				title).setPositiveButton("OK", null).setMessage(msg).create();
+
+		dialog.show();
+	}
+}
